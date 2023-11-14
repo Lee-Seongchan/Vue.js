cf6e0637decd336637d8d8ee7ed759b8bdd56385
lseong96 <lseong96@gmail.com>
2023-11-14 12:00:39 +0900

    vue

---
+++ ch07/src/App.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <h3>ch07. HTTP통신과 JWT</h3>
+
+  <p>
+    <router-link to="/axios">Axios 실습</router-link>
+    <router-link to="/jwt">Jwt 실습</router-link>
+  </p>
+
+  <router-view></router-view>
+</template>
+
+<style scoped></style>
